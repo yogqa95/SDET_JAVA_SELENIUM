7c411b8003595f45d9e80c851599c68775907f16
Yogesh Garje <166059430+yogqa95@users.noreply.github.com>
2024-05-04 16:37:23 +0530

    commit this time

--- src/test/java/testCases/HomeTest.java
+++ src/test/java/testCases/HomeTest.java
@@ -1,6 +1,12 @@
 package testCases;
 
+import org.testng.annotations.Test;
 import testBase.WebTestBase;
 
 public class HomeTest extends WebTestBase {
+    @Test
+    public void print()
+    {
+        System.out.println("Hellow i am connect the Git hum to my inteli");
+    }
 }
